93628fefd49c31987101dfdab3f167a70d0f2d62
Hengrui  Wang <u6202156@anu.edu.au>
2021-10-23 19:50:50 +1100

    test running

--- dataset.py
+++ dataset.py
@@ -6,13 +6,19 @@ import cv2
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms as T
 from torchvision import utils
+from torchvision.transforms import functional as F
 
 class TransformData:
-    def __init__(self):
-        return 0
+    def __init__(self,crop=None):
+        self.crop = crop
     
     def __call__(self,image,mask):
-        return 0
+        image = F.to_pil_image(image)
+        mask = F.to_pil_image(mask)
+        if self.crop:
+            i, j, h, w = T.RandomCrop.get_params(image, self.crop)
+            image, mask = F.crop(image, i, j, h, w), F.crop(mask, i, j, h, w)
+        return image, mask
 
 class CellDataset(Dataset):
     """ Cell segmentation dataset
@@ -49,10 +55,16 @@ class CellDataset(Dataset):
         img_name = 't'+mask_name[-7:]
         image = cv2.imread(self.input_path+img_name)
         mask = cv2.imread(self.mask_path+mask_name, 0)
-        print(self.input_path+img_name, mask_name)
+        # print(self.input_path+img_name, mask_name)
         print(image.shape,mask.shape)
 
-        image, mask = self.transform(image, mask)
+        if self.transform:
+            image, mask = self.transform(image, mask)
+
+        # To tensor
+        to_tensor = T.ToTensor()
+        image = to_tensor(image)
+        mask = to_tensor(mask)
 
         # To long tensor
         mask = mask.long()

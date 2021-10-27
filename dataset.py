d0402363f2c1b4e0a589c0d87a4ee00382e978ee
Hengrui  Wang <u6202156@anu.edu.au>
2021-10-28 01:01:46 +1100

    test ml code

--- dataset.py
+++ dataset.py
@@ -54,7 +54,7 @@ class CellDataset(Dataset):
     def __getitem__(self, idx):
         mask_name = self.image_list[idx]
         img_name = mask_name.split('.')[0]+'.png'#t'+mask_name[-7:]
-        image = cv2.imread(self.input_path+img_name)
+        image = cv2.imread(self.input_path+img_name, 0)
         mask = cv2.imread(self.mask_path+mask_name, 0)
         print(self.input_path+img_name, mask_name)
         print(image.shape,mask.shape,np.amax(image))

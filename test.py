ede3c14a7617f2b8ad65c1bcda617a3be28e1575
Hengrui  Wang <u6202156@anu.edu.au>
2021-10-29 00:14:33 +1100

    test test pipeline

--- test.py
+++ test.py
@@ -42,7 +42,7 @@ def compose_pred(pred: torch.tensor, pseudo_shape: tuple, height_padding: int, w
     # target (1,700,1100)
     patch_size = pred[0].shape[0]
     n_batches = pred[0]
-    output = np.zeros(pseudo_shape[0],pseudo_shape[1],2)
+    output = np.zeros((pseudo_shape[0],pseudo_shape[1],2))
     num_H = int(math.ceil(pseudo_shape[0]/patch_size))
     num_W = int(math.ceil(pseudo_shape[1]/patch_size))
     assert num_H*num_W == n_batches

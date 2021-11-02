9bf83145fb533757721700a8a7d4ea4e6b93aca4
Hengrui  Wang <u6202156@anu.edu.au>
2021-11-03 03:16:57 +1100

    mitosis classification

--- train_classification.py
+++ train_classification.py
@@ -73,7 +73,7 @@ if args.weights:
     net = torch.load(args.weights)
     print(">>> Pretrained weights successfully loaded from "+args.weights)
 else:
-    net = VGG11(in_channels=1,num_classes=2)
+    net = VGG_net(in_channels=1,num_classes=2)
     # net = Network()
     
 # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")

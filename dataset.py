5aa35dd5ac8a6c411cf03773ebbb0b7df9d58122
Hengrui  Wang <u6202156@anu.edu.au>
2021-11-03 00:10:38 +1100

    upload mitosis classification dataset

--- dataset.py
+++ dataset.py
@@ -76,5 +76,69 @@ class CellDataset(Dataset):
 
         return image, mask, img_name
 
+class TransformMitosis:
+    def __init__(self,crop=None):
+        # TODO add more operations to improve model performance
+        self.crop = crop
+    
+    def __call__(self,image,mask):
+        image = F.to_pil_image(image)
+        mask = F.to_pil_image(mask)
+        if self.crop:
+            i, j, h, w = T.RandomCrop.get_params(image, self.crop)
+            image, mask = F.crop(image, i, j, h, w), F.crop(mask, i, j, h, w)
+        return image, mask
 
+class MitosisDataset(Dataset):
+    """ Cell segmentation dataset
+        Folder structure:
+        Dataset_folder (train/val/test)
+            |-- images (01)
+                |-- t000.tif
+                |-- t001.tif
+                |-- ...
+            |-- masks (01_GT)
+                |-- SEG
+                    |-- man_seg021.tif
+                    |-- man_seg022.tif
+                    |-- ...
+    """
 
+    def __init__(self, dataset_folder, transform=None):
+        self.input_path = dataset_folder+"images/"
+        self.mask_path = dataset_folder+"masks/"
+        self.image_list = os.listdir(self.mask_path)
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(os.listdir(self.mask_path))
+    
+    def __getitem__(self, idx):
+        mask_name = self.image_list[idx]
+        img_name = mask_name.split('.')[0]+'.png'#t'+mask_name[-7:]
+        image = cv2.imread(self.input_path+img_name, 0)
+        mask = cv2.imread(self.mask_path+mask_name, 0)
+        # print(self.input_path+img_name, mask_name)
+        # print(image.shape,mask.shape,np.amax(image))
+        # cv2.imwrite('/content/test_img.png',image)
+        # cv2.imwrite('/content/test_mask.png',mask)
+        # print(image.shape,mask.shape)
+
+        if self.transform:
+            image, mask = self.transform(image, mask)
+        
+        # image.save('/content/test_img.png')
+        # mask.save('/content/test_mask.png')
+
+        
+        # To tensor
+        to_tensor = T.ToTensor()
+        image = to_tensor(image)
+        mask = to_tensor(mask)
+        # print(image.shape,mask.shape)
+
+        # To long tensor
+        mask = mask.long()
+
+        return image, mask, img_name
---
+++ train_classification.py
@@ -0,0 +1,217 @@
+# Code partially adapted from https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+#                             https://github.com/cosmic-cortex/pytorch-UNet
+
+# TODO Add object-wise criteration(dice-coef, f1) in metrics.py
+# TODO Write test pipeline in test.py
+
+import os
+import csv
+import torch
+import numpy as np
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.functional import cross_entropy
+
+from argparse import ArgumentParser
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+
+from dataset import *
+from utils import *
+from unet.unet import UNet
+
+parser = ArgumentParser()
+parser.add_argument('--train_folder', required=True, type=str)
+parser.add_argument('--val_folder', type=str)
+parser.add_argument('--checkpoint_folder', required=True, type=str)
+
+parser.add_argument('--device', default='cpu', type=str)
+parser.add_argument('--epochs', default=100, type=int)
+parser.add_argument('--batch_size', default=4, type=int)
+parser.add_argument('--save_freq', default=0, type=int)
+# parser.add_argument('--model_name', type=str, default='unet')
+parser.add_argument('--learning_rate', type=float, default=1e-3)
+parser.add_argument('--weight_decay', type=float, default=0)
+parser.add_argument('--crop_size', required=True, type=int, default=256)
+parser.add_argument('--weights', type=str)
+
+args = parser.parse_args()
+
+# Set random seed for consistent result
+seed = 3000
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+
+# Check and create checkpoint path
+mk_dirs(args.checkpoint_folder)
+
+pred_folder = 'pred/'
+mk_dirs(args.checkpoint_folder+pred_folder)
+
+# Decided to save best model based on val_loss or train_loss
+if args.val_folder:
+    saving_target = 'val_loss'
+else:
+    saving_target = 'train_loss'
+
+# crop_size = args.crop_size
+# Define dataloader
+# Training data transform func
+train_tf = TransformData(crop=(args.crop_size,args.crop_size))
+train_dataset = CellDataset(args.train_folder,transform=train_tf)
+train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+if args.val_folder:
+    # Val data transform func
+    val_tf = TransformData(crop=(args.crop_size,args.crop_size))
+    val_dataset = CellDataset(args.val_folder,transform=val_tf)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2)
+
+
+# Define net
+if args.weights:
+    net = torch.load(args.weights)
+    print(">>> Pretrained weights successfully loaded from "+args.weights)
+else:
+    net = UNet(in_channel=1,out_channel=2)
+    
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+net.to(device=args.device)
+
+
+# Define optimizer and criteration
+criterion = cross_entropy#nn.CrossEntropyLoss()#TODO add extra function
+optimizer = optim.Adam(net.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
+
+min_loss = np.inf
+
+execution_log = []
+for epoch in range(1,args.epochs+1):  # loop over the dataset multiple times
+    
+    net.train(True)
+    # dict used to save metrics for every epoch
+    log_dict = {}
+    log_dict['epoch'] = epoch
+    train_running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # Get the inputs; data is a list of [image_batch, mask_batch]
+        X_batch, y_batch, image_name = data
+        # print(X_batch.shape, y_batch.shape)
+        # Send batch to corresponding device
+        X_batch = Variable(X_batch.to(device=args.device))
+        y_batch = Variable(y_batch.to(device=args.device))
+
+        # Zero the parameter gradients
+        optimizer.zero_grad()
+
+        # Forward + Backward + Optimize
+        y_pred = net(X_batch)
+        # y_pred = torch.argmax(y_pred, dim=1)
+        # y_pred = torch.log(y_pred+1e-32)
+        # loss = criterion(y_pred, y_batch)
+        # print(y_pred.shape,y_batch.shape)
+        loss = criterion(y_pred.float(), y_batch[:,0,:,:].long())
+
+        loss.backward()
+        optimizer.step()
+
+        # Free memory space
+        del X_batch, y_batch
+
+        train_running_loss += loss.item()
+    
+    # print statistics
+    # print('[%d] train_loss: %.3f' %
+    #               (epoch, train_running_loss / (i+1)))
+    log_dict['train_loss'] = train_running_loss / (i+1)
+
+
+    # Val data
+    # Disable training first
+    net.train(False)
+    val_running_loss = 0.0
+    if args.val_folder:
+        for i, data in enumerate(val_loader, 0):
+            X_batch, y_batch, image_name = data
+
+            # Send batch to corresponding device
+            X_batch = Variable(X_batch.to(device=args.device))
+            y_batch = Variable(y_batch.to(device=args.device))
+
+            # Predict on val data
+            y_pred = net(X_batch)
+            
+            # Calculate val loss
+            loss = criterion(y_pred.float(), y_batch[:,0,:,:].long())
+
+            del X_batch, y_batch
+
+            val_running_loss += loss.item()
+        
+        # print('[%d] val_loss: %.3f' %
+        #           (epoch, val_running_loss / (i+1)))
+        log_dict['val_loss'] = val_running_loss / (i+1)
+    print(log_dict)
+    scheduler.step(log_dict[saving_target])
+
+    execution_log.append(log_dict)
+
+    # Save best model
+    if log_dict[saving_target]<min_loss:
+        print(saving_target+' improved from '+str(min_loss)+' to '+str(log_dict[saving_target])+', saving model')
+        min_loss = log_dict[saving_target]
+        torch.save(net, args.checkpoint_folder+'best_model.pt')
+    # print(args.checkpoint_folder+'best_model.pt')
+    # Save latest model
+    torch.save(net, args.checkpoint_folder+'latest_model.pt')
+
+    # Save predicted images for every k epochs
+    if args.save_freq > 0:
+        if epoch%args.save_freq == 0:
+            # TODO
+            # print("Saving sample predictions")
+            net.train(False)
+            target_folder = args.checkpoint_folder+pred_folder+str(epoch)+'/'
+            mk_dirs(target_folder)
+            for i, data in enumerate(val_loader, 0):
+                X_batch, y_batch, image_name = data
+                # print(image_name)
+                # Send batch to corresponding device
+                X_batch = Variable(X_batch.to(device=args.device))
+                y_batch = Variable(y_batch.to(device=args.device))
+
+                # Predict on val data
+                y_pred = net(X_batch)
+                y_pred = torch.argmax(y_pred, dim=1)*255
+
+                # detach from gpu
+                X_batch = X_batch.detach().cpu().numpy()
+                y_batch = y_batch.detach().cpu().numpy()
+                y_pred = y_pred.detach().cpu().numpy()
+
+                # for idx in range(y_pred.shape[0]):
+                pred = y_pred[0]
+                name = image_name[0]
+                gt = y_batch[0][0]*255
+                x = X_batch[0]*255
+                x = np.transpose(x,(1,2,0))
+                # print(pred.shape,gt.shape,x.shape,np.amax(pred),np.amax(gt))
+                cv2.imwrite(target_folder+'pred_'+name,pred)
+                cv2.imwrite(target_folder+'gt_'+name,gt)
+                cv2.imwrite(target_folder+'patch_'+name,x)
+
+                del X_batch, y_batch
+                break
+
+# Save log as .csv file for plot generation in future
+keys = execution_log[0].keys()
+with open(args.checkpoint_folder+'log.csv', 'w', newline='')  as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(execution_log)
+
+print('Finished Training')
+

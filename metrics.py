612106c4531e43d8076c42079af73e49d4524431
Hengrui  Wang <u6202156@anu.edu.au>
2021-11-03 11:07:56 +1100

    mitosis classification

--- metrics.py
+++ metrics.py
@@ -6,5 +6,6 @@ def roc_auc_compute_fn(y_preds: torch.Tensor, y_targets: torch.Tensor) -> float:
 
     y_true = y_targets.detach().cpu().numpy()
     y_pred = y_preds.detach().cpu().numpy()
+    print(y_pred.shape,y_true.shape)
     y_pred = np.transpose([pred[:, 1] for pred in y_pred])
     return roc_auc_score(y_true, y_pred)

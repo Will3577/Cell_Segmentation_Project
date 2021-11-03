01b73d29f10a9f423957ad8df7db326a28312809
Hengrui  Wang <u6202156@anu.edu.au>
2021-11-03 11:38:30 +1100

    mitosis classification

--- metrics.py
+++ metrics.py
@@ -7,9 +7,9 @@ def roc_auc_compute_fn(y_preds: torch.Tensor, y_targets: torch.Tensor) -> float:
 
     y_true = y_targets.detach().cpu().numpy()
     y_pred = y_preds.detach().cpu().numpy()
-    print(y_pred.shape,y_true.shape,y_pred)
+    # print(y_pred.shape,y_true.shape,y_pred)
     y_pred = y_pred[:,1]#np.array([pred[1] for pred in y_pred])
-    print(y_pred.shape,y_true)
+    # print(y_pred.shape,y_true)
     return roc_auc_score(y_true, y_pred)
 
 def accuracy_compute_fn(y_preds: torch.Tensor, y_targets: torch.Tensor) -> float:
@@ -18,7 +18,7 @@ def accuracy_compute_fn(y_preds: torch.Tensor, y_targets: torch.Tensor) -> float
     y_preds = torch.argmax(y_preds, dim=1)
     y_pred = y_preds.detach().cpu().numpy()
     # y_pred = 
-    print(y_pred.shape,y_true.shape,y_pred)
+    # print(y_pred.shape,y_true.shape,y_pred)
     # y_pred = y_pred[:,1]#np.array([pred[1] for pred in y_pred])
-    print(y_pred.shape,y_true)
+    # print(y_pred.shape,y_true)
     return accuracy_score(y_true, y_pred)

3df62df96d20324188ba9c9ff825da5648836401
Hengrui  Wang <u6202156@anu.edu.au>
2021-11-04 18:40:55 +1100

    add code to test classification

--- test_classification.py
+++ test_classification.py
@@ -1,35 +1,124 @@
+# Code partially adapted from https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+#                             https://github.com/cosmic-cortex/pytorch-UNet
+
+# TODO Add object-wise criteration(dice-coef, f1) in metrics.py
+# TODO Write test pipeline in test.py
+
+import os
+import csv
 import torch
-from torch.autograd import Variable
-from torchvision import transforms as T
-import cv2
 import numpy as np
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.functional import cross_entropy
+# from sklearn.metrics import roc_auc_score
 
-# predict single image (40x40, single channel), 
-# default device is GPU!
-def pred_single_img(curr:np.array, next:np.array, net, in_channels:int=2, device:str='cuda:0'):
-    net.eval()
-    to_tensor = T.ToTensor()
-    curr = to_tensor(curr)
-    next = to_tensor(next)
-    if in_channels==2:
-        curr = torch.cat((curr, next), 0)
-    curr = curr[None,...]
-    input = Variable(curr.to(device=device))
-    pred = net(input)
-    out_put = pred.detach().cpu().numpy().argmax()
-    return out_put
+from argparse import ArgumentParser
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+
+from dataset import *
+from utils import *
+from models import *
+from metrics import *
+
+parser = ArgumentParser()
+# parser.add_argument('--train_folder', required=True, type=str)
+parser.add_argument('--weights', required=True, type=str)
+parser.add_argument('--val_folder', required=True, type=str)
+parser.add_argument('--checkpoint_folder', required=True, type=str)
+
+parser.add_argument('--device', default='cpu', type=str)
+parser.add_argument('--epochs', default=100, type=int)
+parser.add_argument('--batch_size', default=4, type=int)
+# parser.add_argument('--model_name', type=str, default='unet')
+parser.add_argument('--learning_rate', type=float, default=1e-3)
+parser.add_argument('--weight_decay', type=float, default=0)
+parser.add_argument('--in_channels', type=int, default=1)
+
+args = parser.parse_args()
+
+# Set random seed for consistent result
+seed = 3000
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
 
 
-# curr_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/normal_cell/01_127/curr/(1002, 411)_0.jpg'
-# next_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/normal_cell/01_127/next/(1002, 411)_0.jpg'
+# Decided to save best model based on val_loss or train_loss
+saving_target = 'val_acc'
 
-curr_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/mitosis_tra_pos/01/curr/(362, 153)_1.jpg'
-next_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/mitosis_tra_pos/01/next/(362, 153)_1.jpg'
+# crop_size = args.crop_size
+# Define dataloader
+# Training data transform func
+# train_tf = TransformMitosis()
+# train_dataset = MitosisDataset(args.train_folder,args.in_channels,transform=train_tf)
+# train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
 
-curr = cv2.imread(curr_path, 0)
-next = cv2.imread(next_path, 0)
+# Val data transform func
+val_tf = TransformMitosis(flip_rate=0,mirror_rate=0)
+val_dataset = MitosisDataset(args.val_folder,args.in_channels,transform=val_tf)
+val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2)
 
-weights = '/content/checkpoints_mitosis/mitosis_best_model.pt'
-net = torch.load(weights)
 
-print(pred_single_img(curr,next,net))
+# Define net
+net = torch.load(args.weights)
+print(">>> Pretrained weights successfully loaded from "+args.weights)
+
+
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+net.to(device=args.device)
+
+
+# Define optimizer and criteration
+criterion = cross_entropy
+
+max_auc = -np.inf
+
+execution_log = []
+for epoch in range(1,args.epochs+1):  # loop over the dataset multiple times
+    
+    # net.train(True)
+    # dict used to save metrics for every epoch
+    log_dict = {}
+    log_dict['epoch'] = epoch
+
+    # Val data
+    # Disable training first
+    net.train(False)
+    val_running_loss = 0.0
+    val_auc = 0.0
+    val_acc = 0.0
+    if args.val_folder:
+        for i, data in enumerate(val_loader, 0):
+            X_batch, y_batch, image_name = data
+
+            # Send batch to corresponding device
+            X_batch = Variable(X_batch.to(device=args.device))
+            y_batch = Variable(y_batch.to(device=args.device))
+
+            # Predict on val data
+            y_pred = net(X_batch)
+            y_pred = torch.softmax(y_pred,dim=1)
+            
+            # Calculate val loss
+            loss = criterion(y_pred.float(), y_batch.long())
+            # aucroc = roc_auc_compute_fn(y_pred.float(), y_batch.long())
+            acc = accuracy_compute_fn(y_pred.float(), y_batch.long())
+            del X_batch, y_batch
+
+            val_running_loss += loss.item()
+            # val_auc += aucroc
+            val_acc += acc
+        
+        # print('[%d] val_loss: %.3f' %
+        #           (epoch, val_running_loss / (i+1)))
+        log_dict['val_loss'] = val_running_loss / (i+1)
+        # log_dict['val_auc'] = val_auc / (i+1)
+        log_dict['val_acc'] = val_acc / (i+1)
+    print(log_dict)
+    execution_log.append(log_dict)
+
+print('Finished Testing')
+
--- utils_mitosis.py
+++ utils_mitosis.py
@@ -270,3 +270,41 @@ def generate_normal_imgs(save_dir:str,crop_size:tuple=(40,40),f_name:int=1):
             break
         
     print("total number of cells on track: ", total_imgs)
+
+#---------------------------------use ml model to classify img----------------------------------
+import torch
+from torch.autograd import Variable
+from torchvision import transforms as T
+import cv2
+import numpy as np
+
+# predict single image (40x40, single channel), 
+# default device is GPU!
+def pred_single_img(curr:np.array, next:np.array, net, in_channels:int=2, device:str='cuda:0'):
+    net.eval()
+    to_tensor = T.ToTensor()
+    curr = to_tensor(curr)
+    next = to_tensor(next)
+    if in_channels==2:
+        curr = torch.cat((curr, next), 0)
+    curr = curr[None,...]
+    input = Variable(curr.to(device=device))
+    pred = net(input)
+    out_put = pred.detach().cpu().numpy().argmax()
+    return out_put
+
+
+# curr_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/normal_cell/01_127/curr/(1002, 411)_0.jpg'
+# next_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/normal_cell/01_127/next/(1002, 411)_0.jpg'
+
+curr_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/mitosis_tra_pos/01/curr/(362, 153)_1.jpg'
+next_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/mitosis_tra_pos/01/next/(362, 153)_1.jpg'
+
+curr = cv2.imread(curr_path, 0)
+next = cv2.imread(next_path, 0)
+
+weights = '/content/checkpoints_mitosis/mitosis_best_model.pt'
+net = torch.load(weights)
+
+print(pred_single_img(curr,next,net))
+# ---------------------------------------end--------------------------------------------------

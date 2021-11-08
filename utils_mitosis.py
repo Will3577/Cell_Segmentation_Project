c66ef1fe3d7d195cbf417022c02a3773ef6b53a7
Hengrui  Wang <u6202156@anu.edu.au>
2021-11-08 20:10:04 +1100

    comment unused code

--- utils_mitosis.py
+++ utils_mitosis.py
@@ -297,14 +297,14 @@ def pred_single_img(curr:np.array, next:np.array, net, in_channels:int=2, device
 # curr_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/normal_cell/01_127/curr/(1002, 411)_0.jpg'
 # next_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/normal_cell/01_127/next/(1002, 411)_0.jpg'
 
-curr_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/mitosis_tra_pos/01/curr/(362, 153)_1.jpg'
-next_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/mitosis_tra_pos/01/next/(362, 153)_1.jpg'
+# curr_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/mitosis_tra_pos/01/curr/(362, 153)_1.jpg'
+# next_path = '/content/COMP9517_Project/Sequences_p/mitosis_classification/mitosis_tra_pos/01/next/(362, 153)_1.jpg'
 
-curr = cv2.imread(curr_path, 0)
-next = cv2.imread(next_path, 0)
+# curr = cv2.imread(curr_path, 0)
+# next = cv2.imread(next_path, 0)
 
-weights = '/content/checkpoints_mitosis/mitosis_best_model.pt'
-net = torch.load(weights)
+# weights = '/content/checkpoints_mitosis/mitosis_best_model.pt'
+# net = torch.load(weights)
 
-print(pred_single_img(curr,next,net))
+# print(pred_single_img(curr,next,net))
 # ---------------------------------------end--------------------------------------------------
